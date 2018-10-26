76a88cd69f0df0c25a215d4ad8d14db18bfcaba8
Nick Dean <klick5000@gmail.com>
2018-10-26 16:01:56 -0600

    add comments and fix en error in request

--- js/index.js
+++ js/index.js
@@ -1,39 +1,68 @@
+/**
+ * watchLandingPage()
+ * First event handler that listens for the "submit" on the landing page
+ */
 function watchLandingPage() {
   $('.landing-container').submit(event => { 
     event.preventDefault();
     
     let landingPageQuery = [
-      $('.js-location').val(),
-      $('.js-free-mode').is(":checked"),
+      $('.js-location').val(), // Location the user wants to search
+      $('.js-free-mode').is(":checked"), // Did they check the free-mode box?
     ];
 
     changeToResultPage(landingPageQuery);
   });
 }
 
+/**
+ * changeToResultPage(landingPageQuery)
+ * ~landingPageQuery is an array with LOCATION, and FREE-MODE [true or false]
+ * This changes from the landing page to the results screen
+ * and handles any changes between the two screens
+ */
 function changeToResultPage(landingPageQuery) {
+  // Remove the landing page
   $('.landing-container').empty();
 
+  // Update the logo to reflect the new screen
   $('.logo').removeClass('landing-logo');
   $('.logo').addClass('results-logo');
 
+  // Send the landingPageQuery that we got from the landing page and
+  // make the API request which will display the data on the results
+  // page.
   getLandingPageEventbriteData(landingPageQuery);
-  initializeResultsScreen();
+  // While we are waiting for the results to come in, we can get the
+  // results page ready for all the data.
+  initializeResultsPage();
 }
 
-// initialize results screen
-function initializeResultsScreen() {
+/**
+ * initializeResultsPage()
+ * This first sets up the HTML for the results page
+ */
+function initializeResultsPage() {
   $('.results-container').append(
     `<p>Results will show here</p>
     <p class="loading">Loading.........</p>
     `
   )
 }
-// send GET request to Eventbrite
+
+/**
+ * getLandingPageEventbriteData(landingPageQuery)
+ * ~landingPageQuery is an array with LOCATION, and FREE-MODE [true or false]
+ * Send the GET request to the Eventbrite API to load our first list of results
+ * This function should only get called once, because its the initial data.
+ * We will make another GET request from then on out if the user wants to filter
+ * or change the results.
+ */
 function getLandingPageEventbriteData(landingPageQuery) {
   const params = {
     'location.address': landingPageQuery[0],
     'location.within': '10mi',
+    price: (landingPageQuery[1] ? 'free' : 'paid'),
     token: "JRKAA3O73D" + "DJB47QH5OT",
   }
 
@@ -42,7 +71,7 @@ function getLandingPageEventbriteData(landingPageQuery) {
 
   console.log(url);
 
-  // -FETCH
+  // - GET FETCH request
   fetch(url)
     .then(response => {
       if (response.ok) {
@@ -53,16 +82,28 @@ function getLandingPageEventbriteData(landingPageQuery) {
     .then(responseJson => displayResults(responseJson))
     .catch(err => {
       $('.results-container').append(`<p class="error">Something went wrong: ${err.message}</p>`);
+      console.log(err);
     });
 
 }
 
+
+/**
+ * formatQueryParams(params)
+ * Takes an object of keys and values and turns it into something like:
+ * ?location.address=Denver%2C%20co&location.within=10mi&price=free
+ */ 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
   .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
+/**
+ * displayResults(responseJson)
+ * Takes the response from Eventbrite and displays it on the screen depending
+ * on what data we need.
+ */
 // display results from eventbrite
 function displayResults(responseJson) {
   $('.loading').remove();
@@ -70,23 +111,33 @@ function displayResults(responseJson) {
   console.log(responseJson);
 
   for (let obj in responseJson.events) {
+    let name = responseJson.events[obj].name.text;
+    let logoUrl = (responseJson.events[obj].logo != null ? responseJson.events[obj].logo.url : 'unavailable');
+
+
     $('.results-container').append(`
-    <div class="result-listing">
-      <img src="${responseJson.events[obj].logo.url}" />
-      <h2>Name: ${responseJson.events[obj].name.text}</h2>
+    <div class="result-listing clearfix">
+    <p>${obj}</p>
+      <h2>Name: ${name}</h2>
+      <img class="result-image" src="${logoUrl}" />
       <p>Start time: ${responseJson.events[obj].start.local}</p>
       <p>Url: ${responseJson.events[obj].url}</p>
       <p>Is free: ${responseJson.events[obj].is_free}</p>
-      <p>Description: ${responseJson.events[obj].description.text}</p>
+      <button class="accordion">Click for Description</button>
+      <p class="result-description hidden">Description: ${responseJson.events[obj].description.text}</p>
     </div>
     `);
   }
-
-
 }
+
+
+// TODO: list
 // listen for more results buttons
 // display filter results?
 // listen for submit on filter?
 // listen for click on event to show more events
 
+/**
+ * Entry point for jQuery to listen to the submit button
+ */
 $(watchLandingPage);
