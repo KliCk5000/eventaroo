d6290cf536db78979ff7cdddc2558c5580812a65
Nick Dean <klick5000@gmail.com>
2018-11-01 00:43:21 -0600

    Initial Firebase code

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBVPLzkLRsHH" + "tXkt4o45W9J2u6XOsW0ozc",
+  authDomain: "eventaroo-nd.firebaseapp.com",
+  databaseURL: "https://eventaroo-nd.firebaseio.com",
+  projectId: "eventaroo-nd",
+  storageBucket: "eventaroo-nd.appspot.com",
+  messagingSenderId: "477507081723"
+};
+firebase.initializeApp(config);
